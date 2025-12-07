34a304659d8ba9fc0cf2ff2788a028cc1000c9f2
Joábio Vilela <joabio.vilela@gmail.com>
2025-12-06 23:30:14 -0300

    bug: corrigir texto da mensagem SMS

--- src/main/java/com/joabio/notificacaosms/constante/MensagemConstante.java
+++ src/main/java/com/joabio/notificacaosms/constante/MensagemConstante.java
@@ -2,6 +2,6 @@ package com.joabio.notificacaosms.constante;
 
 public class MensagemConstante {
 
-    public static final String TICKET_REGISTRADO = "\n\nPrezado(a) %s, seus tickets foram recebidos por nossa equipe e serão análisados. Em breve retornaremos o contato.";
+    public static final String TICKET_REGISTRADO = "\n\nPrezado(a) %s, seus tickets foram recebidos por nossa equipe e serão analisados. Em breve retornaremos o contato.";
     
 }
